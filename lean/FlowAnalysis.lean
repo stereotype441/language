a8d895805c5ac7684687d795720ed137163ecb76
Paul Berry <paulberry@google.com>
2026-09-12 00:20:24 +0000

    WIP: joinPromotedTypes proof

--- lean/FlowAnalysis.lean
+++ lean/FlowAnalysis.lean
@@ -1,4 +1,5 @@
 module
-public import FlowAnalysis.PromotionChain
+public import FlowAnalysis.PromotionChain.Basic
+public import FlowAnalysis.PromotionChain.JoinAlgorithm
 public import FlowAnalysis.SimpleTypes
 public import FlowAnalysis.Types
